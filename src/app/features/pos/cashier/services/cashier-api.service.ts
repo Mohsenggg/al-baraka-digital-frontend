e702648b0efabebe9ca8b1233a0de62983749a9d
mohsen gamal <mohse.gamal@gmail.com>
2026-08-27 10:54:15 +0300

    add receipt preview

--- src/app/features/pos/cashier/services/cashier-api.service.ts
+++ src/app/features/pos/cashier/services/cashier-api.service.ts
@@ -65,6 +65,10 @@ export class CashierApiService {
     return this.http.post<void>(`${this.apiUrl}/${id}/print`, {});
   }
 
+  public getReceiptPreviewUrl(id: number): string {
+    return `${this.apiUrl}/${id}/preview`;
+  }
+
   // --------- Refill API ---------
   
   public validateRefill(payload: import('../../core/models/pos.models').RefillValidateRequest): Observable<import('../../core/models/pos.models').RefillValidateResponse> {
